b5235a743f0294c84a4a8c63e8d88bfc21991aa0
Rahul Paliwal <rahul.pl@directi.com>
2012-06-06 01:08:32 +0530

    rendering data to templates

--- server/todo.py
+++ server/todo.py
@@ -2,4 +2,4 @@ from google.appengine.ext import db
 
 class Todo(db.Model) :
     todo = db.StringProperty(required=True)
-    date = db.DateProperty(required=True)
+    date = db.TimeProperty(required=True)
