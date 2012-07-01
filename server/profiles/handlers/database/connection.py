12e9028ebae79897244ca46b0c79603958a40e26
Rahul Paliwal <rahul.pl@directi.com>
2012-07-01 14:56:25 +0530

    pulled database out of handlers

--- server/profiles/handlers/database/connection.py
+++
@@ -1,4 +0,0 @@
-from sqlobject import *
-
-print 'connection was imported'
-sqlhub.processConnection = connectionForURI('sqlite:///Users/rahulpaliwal/projects/todo/server/profiles/handlers/database/todo.db')
